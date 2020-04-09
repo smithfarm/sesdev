c1f5f76cf900dd2899409fbb1c06926785073c8d
Nathan Cutler <ncutler@suse.com>
2020-04-09 19:00:54 +0200

    sdf

--- seslib/__init__.py
+++ seslib/__init__.py
@@ -267,8 +267,8 @@ VERSION_OS_REPO_MAPPING = {
         'sles-12-sp3': [
             'http://download.suse.de/ibs/Devel:/Storage:/5.0/images/repo/'
             'SUSE-Enterprise-Storage-5-POOL-x86_64-Media1/',
-        #     'http://download.suse.de/ibs/Devel:/Storage:/5.0/images/repo/'
-        #     'SUSE-Enterprise-Storage-5-POOL-Internal-x86_64-Media/',
+            'http://download.suse.de/ibs/Devel:/Storage:/5.0/images/repo/'
+            'SUSE-Enterprise-Storage-5-POOL-Internal-x86_64-Media/',
         ],
         'sles-15-sp1': [
             'http://download.suse.de/ibs/Devel:/Storage:/6.0/images/repo/'
